e07a3389e4ad6f61806fe8113f730da0a425270d
Rafa Lima <rafael.lima@outlook2.com>
2018-02-07 20:44:41 -0400

    Criacao do projeto base 2

--- Exempo Github/src/br/edu/fmf/modelo/gato.java
+++ Exempo Github/src/br/edu/fmf/modelo/gato.java
@@ -1,5 +1,23 @@
+/*
+ *  Oficina Git
+ *  
+ *  Equipe:    Rafael Lima | Wesley Brito | Franco Melo | Rafael Franco
+ *  Data:      07/02/2017
+ *  Descrição: Classe para representar um gato 
+ */
+
 package br.edu.fmf.modelo;
 
 public class gato {
-
+	
+	// atributos
+	
+	private String cor;
+	private String nome;
+	private int idade;
+	
+	// métodos
+	
+	
+	
 }
